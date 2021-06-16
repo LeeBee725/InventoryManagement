20b62e82be5b20017f1a08fab7143865cc118f66
lsw725 <725lsw@hanmail.net>
2021-06-16 23:37:56 +0900

    Change the enviroment from MFC to UWP and Test a UWP Tutorial

---
+++ IM/MysqlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySqlX.XDevAPI;
+using MySqlX.XDevAPI.Common;
+using MySqlX.XDevAPI.CRUD;
+
+namespace IM
+{
+    class MysqlHelper
+    {
+    }
+}
